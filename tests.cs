fb3e5006569964dd70b73c8c5ef39e9fef9fec02
Matthew Steeples <matthew@ledgerscope.com>
2024-11-24 23:27:22 +0000

    Add project files.

---
+++ tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
